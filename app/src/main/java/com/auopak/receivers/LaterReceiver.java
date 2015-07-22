316e15ef1bbe1a04178c2fd2b532bc55f163d081
Usman Baig <auopak8@gmail.com>
2015-07-22 15:04:52 -0400

    Water Drinking apps

---
+++ app/src/main/java/com/auopak/receivers/LaterReceiver.java
@@ -0,0 +1,22 @@
+package com.auopak.receivers;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.auopak.stayhydrated.MainActivity;
+
+public class LaterReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+		// Set AlarmManager to show again in 10min
+		new AlarmReceiver().setAlarm(context, MainActivity.DEFAULT_POSTPONE_DURATION);
+
+		// Delete the notification
+		NotificationManager nManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+		nManager.cancel(1);
+	}
+}
